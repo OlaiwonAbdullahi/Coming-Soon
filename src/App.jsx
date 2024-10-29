488b2fb585284ca4a88c03b6e94dcb417e76a867
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-29 21:10:08 +0100

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import Rocket from "./assets/rocket.svg";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className=" bg-bg min-h-screen">
+      <div className="">
+        <img src={Rocket} alt="" />
+      </div>
+      <div className="">
+        <h2>Coming Soon</h2>
+        <p>
+          Are you Ready to get something new from us. Then subscribe the news
+          latter to get latest updates?
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default App;
