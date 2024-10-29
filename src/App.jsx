db6361e9cf7e4dd2b0c46f49112dd0d491a530c3
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-29 21:43:22 +0100

    update App.jsx and Form.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import Rocket from "./assets/rocket.svg";
 import Form from "./components/Form";
 const App = () => {
   return (
-    <div className=" bg-bg min-h-screen font-pop flex flex-col justify-center items-center">
+    <div className=" bg-bg min-h-screen font-pop flex flex-col gap-4 justify-center items-center">
       <div className="">
         <img src={Rocket} alt="" className=" h-64 w-72" />
       </div>
       <div className=" flex flex-col gap-2">
         <h2 className="text-center font-bold text-3xl">Coming Soon</h2>
-        <p className=" text-center w-5/6 flex mx-auto">
+        <p className=" text-center w-5/6 flex mx-auto font-light">
           Are you Ready to get something new from us. Then subscribe the news
           latter to get latest updates?
         </p>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,15 @@
 const Form = () => {
   return (
-    <div className=" flex">
-      <div className=" basis-2/3">
-        <input type="text" placeholder="Your Email Address" />
+    <div className=" mx-auto flex gap-6">
+      <div className="basis-2/3 w-96 h-12 items-center bg-primary p-2 rounded-md border border-secondary border-r-4 border-b-4">
+        <input
+          type="text"
+          placeholder="Your Email Address"
+          className="text-sm text-secondary h-8 items-center w-full placeholder:text-secondary focus:outline-none"
+        />
       </div>
-      <div className=" basis-1/3">
-        <button className=" bg-btnColor rounded-md p-2 font-semibold border border-secondary border-r-4 border-b-4 w-32 text-sm">
+      <div className="basis-1/3">
+        <button className="bg-btnColor rounded-md h-12 p-2 font-semibold border border-secondary border-r-4 border-b-4 w-32 text-sm">
           Subscribe
         </button>
       </div>
