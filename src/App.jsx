c252fd5b68a58250578b659696fd67fcdb4ddfe5
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-29 21:12:27 +0100

    update App.jsx and Form.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Rocket from "./assets/rocket.svg";
 const App = () => {
   return (
-    <div className=" bg-bg min-h-screen">
+    <div className=" bg-bg min-h-screen flex flex-col justify-center items-center">
       <div className="">
         <img src={Rocket} alt="" />
       </div>
       <div className="">
-        <h2>Coming Soon</h2>
-        <p>
+        <h2 className="text-center font-bold text-3xl">Coming Soon</h2>
+        <p className=" text-center">
           Are you Ready to get something new from us. Then subscribe the news
           latter to get latest updates?
         </p>
