1cf9e0700ab5e4fb04b08572acaa15961cdac268
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-29 21:45:27 +0100

    update Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,6 @@
 const Form = () => {
   return (
-    <div className=" mx-auto flex gap-6">
+    <div className=" mx-auto flex gap-6 flex-col md:flex-row">
       <div className="basis-2/3 w-96 h-12 items-center bg-primary p-2 rounded-md border border-secondary border-r-4 border-b-4">
         <input
           type="text"
