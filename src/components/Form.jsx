32c20acf7489496819b31fdb4a65af7e3409e545
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-29 21:46:09 +0100

    update Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,7 @@
 const Form = () => {
   return (
     <div className=" mx-auto flex gap-6 flex-col md:flex-row">
-      <div className="basis-2/3 w-96 h-12 items-center bg-primary p-2 rounded-md border border-secondary border-r-4 border-b-4">
+      <div className="basis-2/3 md:w-96 w-full h-12 items-center bg-primary p-2 rounded-md border border-secondary border-r-4 border-b-4">
         <input
           type="text"
           placeholder="Your Email Address"
@@ -9,7 +9,7 @@ const Form = () => {
         />
       </div>
       <div className="basis-1/3">
-        <button className="bg-btnColor rounded-md h-12 p-2 font-semibold border border-secondary border-r-4 border-b-4 w-32 text-sm">
+        <button className="bg-btnColor rounded-md h-12 p-2 font-semibold border border-secondary border-r-4 border-b-4 md:w-32 w-full text-sm">
           Subscribe
         </button>
       </div>
