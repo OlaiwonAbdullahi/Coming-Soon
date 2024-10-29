750a80a666473bd7747e472fb69f47ae0c86e2d3
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-29 21:50:28 +0100

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
         <h2 className="text-center font-bold text-3xl">Coming Soon</h2>
         <p className=" text-center w-5/6 flex mx-auto font-light">
           Are you Ready to get something new from us. Then subscribe the news
-          latter to get latest updates?
+          letter to get latest updates?
         </p>
       </div>
       <div className="">
